8f4081bc19d9ac0dfdc1c25bef57f5238d421522
meiniepan <meiniepan@163.com>
2017-03-23 21:07:33 +0800

    RxBinding

--- app/src/main/java/com/example/fs/test/FirstActivity.java
+++ app/src/main/java/com/example/fs/test/FirstActivity.java
@@ -2,19 +2,25 @@ package com.example.fs.test;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Solang on 2017/3/21.
  */
 
-public class FirstActivity  extends AppCompatActivity{
-    Toolbar mToolbar;
+public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Toolbar mToolBar;
+    /**
+     * sdff
+     */
+    private Button mBt;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +29,39 @@ public class FirstActivity  extends AppCompatActivity{
         findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(FirstActivity.this,MainActivity.class));
+                startActivity(new Intent(FirstActivity.this, MainActivity.class));
             }
         });
-        mToolbar.setTitle(R.string.button);
-        mToolbar.setSubtitle(R.string.button);
-        mToolbar.setNavigationIcon(R.mipmap.ic_launcher);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        mToolBar.setTitle(R.string.button);
+        mToolBar.setSubtitle(R.string.button);
+        mToolBar.setNavigationIcon(R.mipmap.ic_launcher);
+        mToolBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.e("TAG", "setNavigationOnclickListener");
             }
         });
+
+        //rxjava 防止按钮重复点击
+        
+
+
     }
 
     private void initView() {
-        mToolbar = (Toolbar) findViewById(R.id.toolBar);
+        mToolBar = (Toolbar) findViewById(R.id.toolBar);
+        mBt = (Button) findViewById(R.id.bt);
+        mBt.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt:
+                break;
+        }
+    }
+
 
 //    @Override
 //    public int getLayoutId() {
